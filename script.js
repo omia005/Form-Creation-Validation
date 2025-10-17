209ee42fe74e24553fe712af8edb58db5ed5e573
Evans Omia <evansomia005@gmail.com>
2025-10-17 17:14:13 +0400

    Update script.js by writing code for the event listeners

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){
 
+  const form = document.getElementById('registration-form');
+  const feedbackDiv = document.getElementById('form-feedback');
+
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const usernameInput = document.getElementById('username');
+    const emailInput = document.getElementById('email');
+    const password = document.getElementById('password');
+  
+    const username = usernameInput.value.trim();
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+  })
+})
